8b5bde3bf3c7cdab58cdc9650b3978b1eec2e5a5
Reid3002 <reid3002@gmail.com>
2023-11-09 14:37:59 -0300

    Fix de animacion de Muerte de Lisarthy
    Ademas de ajustar la animacion, tambien añadi un metodo en la clase PlayerHealth para matar al jugador presionando 2 veces la tecla K asi poder probar la animacion

--- Michigame/Assets/Scripts/Player/PlayerHealth.cs
+++ Michigame/Assets/Scripts/Player/PlayerHealth.cs
@@ -7,6 +7,10 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] private int health;
     // Start is called before the first frame update
 
+    private void Update()
+    {
+        Test();
+    }
     public void TakeDamage(int damage)
     {
         if (health > 0)
@@ -16,7 +20,7 @@ public class PlayerHealth : MonoBehaviour
         }
         else if (health <= 0)
         {
-            gameObject.GetComponent<Animator>().SetBool("dead", true);
+            gameObject.GetComponent<Animator>().SetTrigger("dead");
         }
         
     }
@@ -24,5 +28,13 @@ public class PlayerHealth : MonoBehaviour
     public void Death()
     {
         Destroy(this.gameObject);
+    }
+
+    public void Test()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            TakeDamage(11);
+        }
     }
 }
